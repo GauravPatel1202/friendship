2439b53082d3eab63d67ce80ca2e902c541c822c
Gaurav Patel (c) <patelga@vmware.com>
2021-01-25 01:36:36 +0530

    Update Code For new

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-const routes: Routes = [];
+import { ErrorpageComponent } from './error/errorpage/errorpage.component';
+import { PageNotFoundComponent } from './error/page-not-found/page-not-found.component';
+const routes: Routes = [{
+    path: '',
+    pathMatch: 'full',
+    loadChildren: () => import('src/app/home/home.module').then(m => m.HomeModule),
+  },
+  { path: 'no_access', component: ErrorpageComponent },
+  { path: '**', component: PageNotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
